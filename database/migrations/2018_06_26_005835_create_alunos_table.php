c2cc00fe62d8678ddca061cc577de6cc581f0109
dariosantos <dariobennaia@fabricainfo.com>
2018-06-26 01:29:31 +0000

    migrates e model criados

---
+++ database/migrations/2018_06_26_005835_create_alunos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlunosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alunos', function (Blueprint $table) {
+            $table->increments('id');            
+            $table->string('nome_aluno');
+            $table->integer('curso');            
+            $table->integer('numero_maricula');            
+            $table->integer('semestre');        
+            $table->enum('status', ['matriculado', 'trancado','jubilado']);    
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alunos');
+    }
+}
