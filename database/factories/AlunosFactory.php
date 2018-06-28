721c420070837f209c35effb634fbde7bf140e5a
Demontie <demontiecosta17@gmail.com>
2018-06-27 23:23:15 -0300

    factory curso adicionado;

--- database/factories/AlunosFactory.php
+++ database/factories/AlunosFactory.php
@@ -3,13 +3,13 @@
 use Faker\Generator as Faker;
 
 $factory->define(\App\Alunos::class, function (Faker $faker) {
+    $arraystatus = array_rand(array("matriculado", "trancado", "jubilado"));
+    $valor = $arraystatus[0];
     return [
-
-
     'nome_aluno' => $faker->name,
     'numero_maricula' => rand(),
     'semestre' => rand(1,2),
-    'status' => array_rand(array("matriculado", "trancado", "jubilado")),
+    'status' => $valor,
     'curso' => function () {
             return factory(\App\Cursos::class)->create()->id;
         }
--- database/factories/CursosFactory.php
+++ database/factories/CursosFactory.php
@@ -4,6 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Cursos::class, function (Faker $faker) {
     return [
-        //
+        'nome_curso' => $faker->name,
+        'instituicao_ensino' => $faker->name,
+        'cod_curso' => rand(),
     ];
 });
