a63ced7dbcc30c179b8cea58c88859b7b17379e8
Demontie Costa <demontiecosta17@gmail.com>
2018-06-29 09:07:42 -0300

    db seed adicionado e arquivo de instalacao modificado

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 CRUD simples de cadastro de aluno e curso, sujeito a modificação.
 
-# Instalação
-Requisitos <br>
+
+#Requisitos <br>
 PHP >= 7.1.3 <br>
 OpenSSL PHP Extension <br>
 PDO PHP Extension <br>
@@ -14,3 +14,7 @@ Ctype PHP Extension <br>
 JSON PHP Extension <br>
 Docker - https://docs.docker.com/install/
 
+# Instalação
+Apos projeto clonado e docker instalado utilizar o comando "docker-compose up -d" para preparar o ambiente<br>
+Configure o arquivo .env para as configurações do seu container
+ 
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('cursos')->insert([
+            'cod_curso'  => '123456',
+            'nome_curso' => 'Curso de pedagogia',
+            'instituicao_ensino' => 'Faculdade alguma coisa'
+        ]);
+        DB::table('alunos')->insert([
+            'nome_aluno'  => '123456',
+            'curso' => 1,
+            'numero_maricula' => 'Faculdade alguma coisa',
+            'semestre' => 'Faculdade alguma coisa',
+            'status' => 'matriculado'
+        ]);
     }
 }
